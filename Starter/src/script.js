fb90286cdd4a85020e865a1261fdc3dd24eef417
TLAMHutto <keatonhutto8@gmail.com>
2022-05-15 23:22:44 -0500

    transfrom

--- Starter/src/script.js
+++ Starter/src/script.js
@@ -1,1 +1,46 @@
 import './style.css'
+import * as THREE from 'three'
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const group = new THREE.Group()
+scene.add(group)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube2.position.x = 3
+cube3. position.y = 2
+group.position.z = -5
+group.add(cube1, cube2, cube3)
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('canvas.webgl')
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
